fedc4d0bd6aae2ef3c0a6ea01427b76f4799f6fd
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-04-15 11:15:02 +0530

    str

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState, Component } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">Movie Database</h1>
+      <p data-ns-test="project-name">
+        descriptions of movies and allows visually impaired people to follow a
+        movie along with their peers
+      </p>
+    </div>
+  );
+};
 
 export default App;
